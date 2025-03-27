57b87b48c1cf6887abf8ce3deb68386719d06903
Ice Patipan <patipan.roungsuwan@gmail.com>
2025-03-27 16:44:09 +0700

    ci: add global.css to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./styles/globals.css"
   ],
   theme: {
     extend: {},
